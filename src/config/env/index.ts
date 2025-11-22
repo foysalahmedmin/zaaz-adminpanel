7d5f9fc10155f6e298b41124164880a7b52d1f73
foysalahmedmin <foysalahmedmin@gmail.com>
2025-11-22 10:58:17 +0600

    -

--- src/config/env/index.ts
+++ src/config/env/index.ts
@@ -1,6 +1,5 @@
 export const ENV = {
-  api_url:
-    (import.meta.env.VITE_API_URL as string) || "http://localhost:5000",
   app_url: (import.meta.env.VITE_APP_URL as string) || "http://localhost:8080",
+  api_url: (import.meta.env.VITE_API_URL as string) || "http://localhost:5000",
   environment: import.meta.env.MODE as "development" | "production",
 };
